a404fa688444d2dd4484423f796c9fc82974579f
fabian <42480199+fbientrigo@users.noreply.github.com>
2023-06-04 19:30:16 -0400

    Delete deepql directory

--- deepql/init_workspace.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def create_workspace():
-    folder_names = ['data', 'errors', 'evolution_force', 'gen_data', 'model']
-    
-    for folder_name in folder_names:
-        if not os.path.exists(folder_name):
-            os.mkdir(folder_name)
